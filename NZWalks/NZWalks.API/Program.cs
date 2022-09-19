3ec3883bd84003a9634af48e5069824211dc1ede
David Silva <david_goncalvessilva@hotmail.com>
2022-09-19 19:07:52 +0100

    End of section 3 (Create New Controller)

--- NZWalks/NZWalks.API/Program.cs
+++ NZWalks/NZWalks.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using NZWalks.API.Data;
+using NZWalks.API.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,6 +16,12 @@ builder.Services.AddDbContext<NZWalksDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
 });
+
+// Inject Region Repository
+builder.Services.AddScoped<IRegionRepository, RegionRepository>();
+
+// Inject AutoMapper (Profiles for DTO)
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 var app = builder.Build();
 
